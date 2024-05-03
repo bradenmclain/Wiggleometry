c63830e29c5c00d286215bdca55831e4d23ae230
Mclain <btmywv@umsystem.edu>
2024-05-03 14:51:47 -0500

    classifier does the do

--- Wiggleometer3.py
+++ Wiggleometer3.py
@@ -10,8 +10,11 @@ class Wiggleometer:
         self.start_time = time.time()
         self.video = cv2.VideoCapture(path)
         self.state, self.frame = self.video.read()
-        self.binary_ring_buffer = collections.deque(maxlen = 10)
+        self.pixel_count_buffer = collections.deque(maxlen = 15)
         self.binary_image_ring_buffer = collections.deque(maxlen = 2)
+        self.frame_change_buffer = collections.deque(maxlen = 15)
+        self.frame_change_buffer.append(1)
+        self.state = "Empty"
         
 
         self.pixel_count = 0
@@ -38,20 +41,19 @@ class Wiggleometer:
         self.color_binary_image[np.where(red>colors['red'])] = 255
         self.color_binary_image[np.where(green>colors['green'])] = 255
         self.color_binary_image[np.where(blue>colors['blue'])] = 255
-        self.pixel_count = np.sum(self.color_binary_image)
-        self.binary_image_ring_buffer.append(self.color_binary_image)
 
         return 
     
-    def gray_threshold(self,threshold):
+    def gray_threshold(self):
+        threshold = 252
         gray_image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY) 
         self.binary_image = np.zeros_like(gray_image)
         self.binary_image[np.where(gray_image>threshold)] = 255
         self.pixel_count = np.sum(self.binary_image)
 
     def binary_pixel_count_look_back(self):
-        self.binary_ring_buffer.append(self.pixel_count)
-        return np.mean(self.binary_ring_buffer)
+        self.pixel_count_buffer.append(self.pixel_count)
+        return np.mean(self.pixel_count_buffer)
 
 
     def resize_frame(self):
@@ -89,9 +91,30 @@ class Wiggleometer:
 
         return gray
     
+    def get_state(self):
+        self.resize_frame
+        self.gray_threshold()
+        self.pixel_count_buffer.append(self.pixel_count)
+        self.binary_image_ring_buffer.append(self.binary_image)
+        self.frame_change_buffer.append(np.sum(self.binary_image - self.binary_image_ring_buffer[0],dtype=np.float64))
+        self.frame_change_difference = np.abs(np.diff(np.asarray(self.frame_change_buffer,dtype=np.float64)))
+        if np.mean(self.pixel_count_buffer) > 3000000:
+            self.state = 'Balling'
+        elif np.mean(self.pixel_count_buffer) < 1000:
+            self.state = 'Empty'
+        elif np.mean(self.frame_change_difference) < 250000:
+            self.state = 'Stable'
+        else:
+            self.state = 'Stubbing'
 
+        
+   
+
+    
 def moving_average(x, w):
     return np.convolve(x, np.ones(w), 'valid') / w
+
+
 
 if __name__ == '__main__':
 
@@ -119,45 +142,48 @@ if __name__ == '__main__':
         i+=1
 
         if stable.state:
-            stable.resize_frame()
-            stable.threshold_image(red=200)
-            stable.gray_threshold(252)
+            stable.get_state()
+            deposit_state = stable.state
             stable_pix.append(stable.pixel_count)
-            stable_pix_lookback.append(stubby.binary_pixel_count_look_back())
-            stable_change.append(np.sum(stable.binary_frame_change_count()))
+            stable_change.append(np.mean(stable.frame_change_difference))
+            print(deposit_state)
             stable.get_frame()
 
         if stubby.state:
-            stubby.resize_frame()
-            stubby.threshold_image(red=200)
-            stubby.gray_threshold(252)
+            stubby.get_state()
+            deposit_state = stubby.state
             stubby_pix.append(stubby.pixel_count)
-            stubby_pix_lookback.append(stable.binary_pixel_count_look_back())
-            stubby_change.append(np.sum(stubby.binary_frame_change_count()))
+            stubby_change.append(np.mean(stubby.frame_change_difference))
             stubby.get_frame()
 
         if balling.state:
-            balling.resize_frame()
-            balling.threshold_image(red=200)
-            balling.gray_threshold(252)
+            balling.get_state()
+            deposit_state = balling.state
             balling_pix.append(balling.pixel_count)
-            balling_pix_lookback.append(balling.binary_pixel_count_look_back())
-            balling_change.append(np.sum(balling.binary_frame_change_count()))
+            balling_change.append(np.mean(balling.frame_change_difference))
             balling.get_frame()
-            # cv2.imshow('frame',balling.binary_image)
-            # cv2.waitKey(5)
+            cv2.imshow('frame',balling.binary_image)
+            cv2.waitKey(5)
       
         #start = time.time()
         
         #circles = stable.cv_hough_circle()
-        print(f'Stable is {stable.state} Stubbing is {stubby.state} Balling is {balling.state} Big is {big}', end='\r')
+        #print(f'Stable is {stable.state} Stubbing is {stubby.state} Balling is {balling.state} Big is {big}', end='\r')
         # cv2.imshow('frame',stable.frame)
         # cv2.waitKey(5)
+    
+    plt.plot(stable_pix,'g', label = 'Stable')
+    plt.plot(stubby_pix,'b',label = 'Stubby')
+    plt.plot(balling_pix,'orange', label = 'Balling')
+    plt.xlabel("Frame")
+    plt.ylabel("Total Thresholded Pixel Count")
+    plt.legend(loc='best')
+
+    plt.show()
         
-    stable_change = np.array(stable_change,dtype= np.float64)
-    stubby_change = np.array(stubby_change,dtype= np.float64)
-    print(stable_change[450:500])
-    print(np.diff(stable_change[450:500]))
+    # stable_change = np.array(stable_change,dtype= np.float64)
+    # stubby_change = np.array(stubby_change,dtype= np.float64)
+
 
     plt.plot(stable_change,'g', label = 'Stable')
     plt.plot(stubby_change,'b',label = 'Stubby')
@@ -171,29 +197,13 @@ if __name__ == '__main__':
 
     # plt.plot(np.abs(np.diff(np.array(stable_change))),'g',label = 'Stable Difference')
     # plt.plot(np.abs(np.diff(np.array(stubby_change))),'b',label = 'Stubby Difference')
-    plt.plot(moving_average(np.abs(np.diff(np.array(stable_change))),10),'g',label = 'Stable')
-    plt.plot(moving_average(np.abs(np.diff(np.array(stubby_change))),10),'b',label = 'Stubby')
-    plt.legend(loc='best')
-    plt.xlabel("Frame")
-    plt.ylabel("Rate of Change of Current Frame - Previous Frame")
-    plt.show()
-    plt.clf()
-
-    plt.plot(stable_pix_lookback,'g', label = 'Stable')
-    plt.plot(stubby_pix_lookback,'b',label = 'Stubby')
-    plt.plot(balling_pix_lookback,'orange', label = 'Balling')
-    plt.xlabel("Frame")
-    plt.ylabel("Total Thresholded Pixel Count")
-    plt.legend(loc='best')
-
-    plt.show()
-
-    plt.clf()
-
-    plt.plot(stable_pix,'g')
-    plt.plot(stubby_pix,'b')
-    plt.plot(balling_pix,color='orange')
-    plt.show()
+    # plt.plot(moving_average(np.abs(np.diff(np.array(stable_change))),10),'g',label = 'Stable')
+    # plt.plot(moving_average(np.abs(np.diff(np.array(stubby_change))),10),'b',label = 'Stubby')
+    # plt.legend(loc='best')
+    # plt.xlabel("Frame")
+    # plt.ylabel("Rate of Change of Current Frame - Previous Frame")
+    # plt.show()
+    # plt.clf()
 
     
             
