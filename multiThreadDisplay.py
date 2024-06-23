062f3afd56027aa370d038cc7378f1b5e5d7a9fa
Braden McLain <bradenm@mopine.com>
2024-06-23 17:46:49 -0500

    works, should be cleaned up though

--- multiThreadDisplay.py
+++ multiThreadDisplay.py
@@ -17,11 +17,6 @@ class VideoWidget():
         self.cap.set(cv2.CAP_PROP_FPS, 30)  
 
         self.state, self.frame = self.cap.read()
-
-
-        self.i = 0
-
-
         self.queue = queue
 
 
@@ -30,21 +25,18 @@ class VideoWidget():
 
 
     def fill_video_queue(self):
-        self.queue.put(self.frame)
-
-    
-    def close_program(self):
-        os.remove(f'output{self.i}.avi')
-        self.cap.release()
+        self.queue.put(np.copy(self.frame))
+        print('frame def put into queue')
 
 
 def save_video_queue(image_queue,flag_queue):
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(f'output{time.time()}.avi', fourcc, 30.0, (1920, 1080))
-    flag = ''
+    flag = 'idle'
     
     while flag != 'close':
         ic(flag)
+        
         if flag_queue.empty() == False:
             flag = flag_queue.get()
         
@@ -65,10 +57,18 @@ def save_video_queue(image_queue,flag_queue):
                 else:
                     ic('empty frame recieved while finalizing recording')
             out.release()
-            out = cv2.VideoWriter(f'output{time.time()}.avi', fourcc, 30.0, (1920, 1080))
+            name = time.time()
+            out = cv2.VideoWriter(f'output{name}.avi', fourcc, 30.0, (1920, 1080))
 
-        time.sleep(1)
+        time.sleep(.1)
+
     
+    out.release()
+    try:
+        os.remove(f'output{name}.avi')
+    except:
+        pass
+    ic('closing')
 
 
 if __name__ == '__main__':
@@ -79,56 +79,57 @@ if __name__ == '__main__':
     flag_queue = Queue()
     image_queue = Queue()
     flag = 'idle'
-    i=0
-    key = 'hi'
+    key = ''
     
 
-    stream = VideoWidget(image_queue)
-    #schedule.every(1).seconds.do(stream.save_video_queue)
-    
+    stream = VideoWidget(image_queue)   
     
     if stream.cap.isOpened(): # try to get the first frame
         stream.get_frame()
         process = Process(target=save_video_queue, args=(image_queue, flag_queue))
-        process.start()        
+        process.start()
+
     else:
         stream.state = False
 
-    while stream.state:        
-        start2 = time.time()
-        
+    while stream.state:               
         if key == 27: # exit on ESC
+            record = False
             if flag == 'recording':
-                flag == 'stop_recording'
+                flag = 'stop_recording'
                 flag_queue.put(flag)
 
-            stream.close_program()
-            break
+            flag = 'close'
+            flag_queue.put(flag)
+            stream.cap.release()
+            stream.state = False
 
         if key == ord('r') or record:
             record = True
-            flag = "recording"
-            flag_queue.put(flag)
+            if flag != 'recording':
+                flag = "recording"
+                flag_queue.put(flag)
+            flag = 'recording'
             start = time.time()
+            print('frame put into recording')
             stream.fill_video_queue()
             print(f'recording took {time.time()-start} seconds')
             
         if key == ord('s'):
             record = False
-            flag = "stop"
+            flag = "stop_recording"
             flag_queue.put(flag)
-            print('hitting')
+            flag = 'idle'
+            flag_queue.put(flag)
 
         display_frame = cv2.putText(stream.frame,flag,(10,140), font, 2, (255,255,255), 2, cv2.LINE_AA)
         cv2.imshow("preview", stream.frame)
         key = cv2.waitKey(1)
-        prev_frame = stream.frame
-        stream.get_frame()
-        print(np.sum(stream.frame-prev_frame))
-        if np.sum(stream.frame-prev_frame) ==0:
-            print('SHOWING NOTHING')
-        i+=1
-        print(f'on frame {i}')
+        if flag != 'close':
+            stream.get_frame()
 
 
+    process.join()
+    process.close()
     cv2.destroyWindow("preview")
+    stream.cap.release()
