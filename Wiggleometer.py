207d1562ccefbb6c54f0c81f5d5fe8777d7992f9
Braden McLain <bradenm@mopine.com>
2024-08-20 11:24:42 -0500

    spacing is super close

--- Wiggleometer.py
+++ Wiggleometer.py
@@ -284,18 +284,14 @@ def find_stub_indecies(binary_change,engage_index,retract_index):
     der_peaks = []
     der_valleys = []
 
-    for i in range(1, len(x_org)):
-        if der[i-1] * der[i] < 0:  # Sign change detected
+    for i in range(1, len(xnew)):
+        if new_der[i-1] * new_der[i] < 0:  # Sign change detected
             # Step 2: Refine the zero crossing using root_scalar
-            root_result = root_scalar(der_interp_function, bracket=[x_org[i-1], x_org[i]])
+            root_result = root_scalar(der_interp_function, bracket=[xnew[i-1], xnew[i]])
             if root_result.converged:
                 zero_crossings.append(root_result.root)
 
  
-    #plt.plot(x_org,der,color = 'blue')
-    # plt.plot(peaks, binary_change[peaks], "x")
-    # plt.plot(binary_change,color = 'green')
-    # plt.axhline(0, color='black', linestyle='--', lw=0.8)  # Add a horizontal line at y=0
     for zero_crossing in zero_crossings:
         if sec_der_interp_function(zero_crossing) >= 0:
             #plt.scatter(zero_crossing, der_interp_function(np.array(zero_crossing)), color='red', zorder=5, label='Zero Crossings')
@@ -304,15 +300,6 @@ def find_stub_indecies(binary_change,engage_index,retract_index):
             #plt.scatter(zero_crossing, der_interp_function(np.array(zero_crossing)), color='blue', zorder=5, label='Zero Crossings')
             der_peaks.append(zero_crossing)
 
-
-
-        
-
-    # plt.legend()
-    # plt.show()
-
-    # mask = ~np.isin(post_process_deposit_peaks, trim_index+engage_index+retract_index)
-    # stubs = peaks[mask]
 
     if len(engage_index) > 0:
         stubs = stubs[(np.max(engage_index) < stubs)]
@@ -325,37 +312,35 @@ def find_stub_indecies(binary_change,engage_index,retract_index):
     for peak in stubs:
 
         left = int(np.where(((np.diff(np.sign(der_valleys-peak)) != 0)*1)==1)[0])
+        print(f'earlier left is {left}')
         if left != None:
-            right = int(left + 1)
+            print(f'left is {left}')
+            right = (left + 1)
             #print(peak)
             #print(der_valleys[int(left)],der_valleys[int(right)])
             peak_time = der_valleys[int(right)] - der_valleys[int(left)]
             print(f'peak lasted {peak_time/30} seconds')
-            # plt.plot(der_valleys[int(left)], interp_function(der_valleys[int(left)]), color='red', marker=".")
-            # plt.plot(der_valleys[int(right)], interp_function(der_valleys[int(right)]), color='green', marker='.')
-            # plt.plot(peak,interp_function(peak),marker='.',color='black')
 
-            local_ynew = np.arange(interp_function(der_valleys[int(right)]),interp_function(der_valleys[int(left)]),.1)
-            #print(local_ynew)
-            #print(f'the length is {len(local_ynew)}')
+            local_x = np.arange((der_valleys[int(left)]),(der_valleys[int(right)]),.01)
+            local_y = interp_function(local_x)
 
 
             if interp_function(der_valleys[int(left)]) > interp_function(der_valleys[int(right)]):
-                local_ynew = np.linspace(interp_function(round(der_valleys[int(right)])),interp_function(round(der_valleys[int(left)])),20)
-                local_inverse_interp_function = interpolate.interp1d(binary_change[round(der_valleys[int(left)]):round(der_valleys[int(right)])],x_org[round(der_valleys[int(left)]):round(der_valleys[int(right)])])
-
-                vals = local_inverse_interp_function(local_ynew)
-                zero_vals = (vals- der_valleys[int(left)])
-                plt.plot(zero_vals)
-                plt.show()
+                plt.plot(der_valleys[int(left)],interp_function(der_valleys[int(left)]),marker='*',color = 'blue')
+                new_point = (np.where(((np.diff(np.sign(local_y-interp_function(der_valleys[int(left)]))) != 0)*1)==1))
+                plt.plot(local_x[new_point[-1]],local_y[new_point[-1]])
+                print(new_point)
                 pass
             else:
-                #print('right was greater')
+                plt.plot(der_valleys[int(right)],interp_function(der_valleys[int(right)]),marker='*',color = 'purple')
+                new_point = (np.where(((np.diff(np.sign(local_y-interp_function(der_valleys[int(right)]))) != 0)*1)==1))
+                plt.plot(local_x[new_point[0]],local_y[new_point[0]])
                 pass
 
-
-    plt.plot(binary_change)
-    plt.show()
+            
+            plt.plot(binary_change)
+            plt.plot(local_x,local_y,color='green')
+            plt.show()
 
 
 
@@ -370,62 +355,6 @@ def find_stub_indecies(binary_change,engage_index,retract_index):
     
     return stubs
 
-def find_stub_lengths(binary_change,stub_indecies,engage_index,retract_index):
-    der = np.gradient((binary_change))
-    sec_der = np.gradient(der) 
-    #sec_der = (sec_der**2) * np.sign(sec_der)
-    #sec_der = moving_average(sec_der,4)
-
-    maxima,__ = find_peaks(sec_der,prominence=10000000000)
-    minima,__ = find_peaks(sec_der*-1,prominence=10000000000)
-    maxima_prominences = peak_prominences(sec_der, maxima)[0]
-
-    maxima = np.asarray(maxima)
-    minima = np.asarray(minima)
-    print(f'the original length was {len(maxima)}')
-
-
-
-    if len(engage_index) > 0:
-        maxima = maxima[(np.max(engage_index) < maxima)]
-        
-
-    if len(retract_index) > 0:
-        maxima = maxima[(maxima < np.min(retract_index))]
-    print(f' the trimmed length is {len(maxima)}')
-
-    if len(engage_index) > 0:
-        minima = minima[(np.max(engage_index) < minima)]
-        
-
-    if len(retract_index) > 0:
-        minima = minima[(minima < np.min(retract_index))]
-    print(f' the trimmed length is {len(maxima)}')
-
-    for peak in maxima:
-        plt.plot(peak,binary_change[peak],'*',color='red')
-
-    for peak in minima:
-        plt.plot(peak,binary_change[peak],'*',color='green')
-    
-    plt.plot(binary_change)
-    plt.plot(sec_der,color='orange')
-
-    # for peak in minima:
-    #     plt.plot(peak-1,binary_change[peak-1],'*',color = 'green')
-
-
-    deposit_maxima = []
-
-
-
-    for peak in stub_indecies:
-        plt.plot(peak,binary_change[peak],'*',color = 'black')
-
-    plt.show()
-    
-    
-    pass
 
 if __name__ == '__main__':
 
